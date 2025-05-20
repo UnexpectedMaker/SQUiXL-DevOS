469bd6742815aff06b5e19d6cfac7f4aeb9ba6b7
unknown <optoisolated@outlook.com>
2025-05-21 01:12:10 +1000

    Slashdot RSS Widget concept

---
+++ platformio/src/ui/widgets/widget_articles.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "ui/ui_window.h"
+
+struct ARTICLE
+{
+    String headline;
+    String date;
+    String creator;
+    String subject;
+    String comments;
+
+    ARTICLE(String h, String d, String c, String s, String co) : headline(h), date(d), creator(c), subject(s), comments(co) {}
+};
+
+class widgetArticles : public ui_window
+{
+public:
+    bool redraw(uint8_t fade_amount, int8_t tab_group = -1) override;
+    bool process_touch(touch_event_t touch_event);
+
+    void process_article_data(bool success, const String &response);
+
+    void show_next_article();
+
+private:
+    std::string server_path = "https://rss.slashdot.org/slashdot/slashdotmain";
+    unsigned long next_update = 0;
+    unsigned long next_article_swap = 0;
+
+    bool should_redraw = false;
+    bool is_setup = false;
+    bool process_next_article = false;
+    bool is_getting_more_articles = false;
+
+    uint8_t char_width = 0;
+    uint8_t char_height = 0;
+    uint8_t max_chars_per_line = 0;
+    uint8_t max_lines = 0;
+
+    std::vector<String> lines;
+
+    std::vector<ARTICLE> stored_articles;
+
+    BB_SPI_LCD _sprite_article;
+
+    void process_lines();
+    void wrap_text(const String &text, int max_chars_per_line);
+    void get_char_width();
+
+    void reset_refresh_timer();
+};
+
+extern widgetArticles widget_articles;

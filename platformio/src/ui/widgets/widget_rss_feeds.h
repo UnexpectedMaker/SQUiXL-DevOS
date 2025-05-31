1d0a4d67572b933a317643b86eac795b4a429982
unknown <optoisolated@outlook.com>
2025-05-31 12:36:27 +1000

    SSID can be viewed with a tap, RSS feed improvements.

--- platformio/src/ui/widgets/widget_rss_feeds.h
+++ platformio/src/ui/widgets/widget_rss_feeds.h
@@ -4,53 +4,53 @@
 #include <vector>
 #include <string>
 
-struct ARTICLE
-{
-		String headline;
-		String date;
-		String creator;
-		String subject;
-		String comments;
+struct ARTICLE {
+    std::string headline;
+    std::string date;
+    std::string creator;
+    std::string subject;
+    std::string comments;
 
-		ARTICLE(String h, String d, String c, String s, String co) : headline(h), date(d), creator(c), subject(s), comments(co) {}
+    ARTICLE(const std::string& h, const std::string& d, const std::string& c,
+            const std::string& s, const std::string& co)
+        : headline(h), date(d), creator(c), subject(s), comments(co) {}
 };
 
-class widgetRSSFeeds : public ui_window
-{
-	public:
-		bool redraw(uint8_t fade_amount, int8_t tab_group = -1) override;
-		bool process_touch(touch_event_t touch_event);
-		void process_article_data(bool success, const String &response);
-		void show_next_article();
+class widgetRSSFeeds : public ui_window {
+public:
+    bool redraw(uint8_t fade_amount, int8_t tab_group = -1) override;
+    bool process_touch(touch_event_t touch_event);
+    void process_article_data(bool success, const String& response);
+    void show_next_article();
 
-	private:
-		std::string feed_url = "";
-		unsigned long next_update = 0;
-		unsigned long next_article_swap = 0;
+private:
+    std::string feed_url = "";
+    unsigned long next_update = 0;
+    unsigned long next_article_swap = 0;
 
-		bool should_redraw = false;
-		bool is_setup = false;
-		bool process_next_article = false;
-		bool is_getting_more_articles = false;
+    bool is_debug = true;
 
-		uint8_t char_width = 0;
-		uint8_t char_height = 0;
-		uint8_t max_chars_per_line = 0;
-		uint8_t max_lines = 0;
+    bool should_redraw = false;
+    bool is_setup = false;
+    bool process_next_article = false;
+    bool is_getting_more_articles = false;
 
-		std::vector<String> lines;
+    uint8_t char_width = 0;
+    uint8_t char_height = 0;
+    uint8_t max_chars_per_line = 0;
+    uint8_t max_lines = 0;
 
-		std::vector<ARTICLE> stored_articles;
+    std::vector<String> lines;
 
-		BB_SPI_LCD _sprite_article;
+    std::vector<ARTICLE> stored_articles;
 
-		void process_lines();
-		// void wrap_text(const String &text, int max_chars_per_line);
+    BB_SPI_LCD _sprite_article;
 
-		void reset_refresh_timer();
+    void process_lines();
+    void reset_refresh_timer();
 
-		unsigned long parse_iso8601_date(const String &date_str);
-		String format_time_ago(unsigned long seconds_ago);
+    unsigned long parse_date(const std::string& date_str);
+    std::string format_time_ago(unsigned long seconds_ago);
 };
 
 extern widgetRSSFeeds widget_rss_feeds;
